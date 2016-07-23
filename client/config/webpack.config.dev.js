aa595dd73f38ce63ca933a759a6ced69610fcfb4
Kawin Glomjai <kawin.g@taskworld.com>
2016-07-23 23:06:55 +0700

    infrastructure

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -1,3 +1,46 @@
 const webpack      = require('webpack')
 const path         = require('path')
 const autoprefixer = require('autoprefixer')
+
+//resolve path files.
+const SOURCE_PATH = path.resolve(__dirname, '..', 'src')
+const BUILD_PATH = path.resolve(__dirname, '..', 'build')
+
+//export webpack configuration
+module.export = {
+  devtool: 'eval-cheap-module-source-map',
+  entry: {
+    travel_bylon: [ 'babel-polyfill', './index.js' ]
+  },
+  output: {
+    //no required in dev mode, nonetheless it will be crashes without them.
+    path: BUILD_PATH,
+    pathinfo: true,
+    filename: `[name]${development ? '' : ''}.js`,
+    publicPath: 'http://localhost:2000/asserts/'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: SOURCE_PATH,
+      }
+    ],
+    loaders: [
+      {
+        test: /\.(css)$/,
+        loaders: [ 'style', 'css' ],
+      },
+      {
+        test: /\.(styl)$/,
+        loaders: [ 'style', 'css', 'postcss', 'stylus' ]
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel?cacheDirectory',
+      }
+    ],
+  },
+}
