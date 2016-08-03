d84033e7055d6179dd4ea528a86facc85e29d4fe
Kawin Glomjai <kawin.g@taskworld.com>
2016-08-03 12:59:03 +0700

    Redux Provider

--- client/src/action/index.js
+++ client/src/action/index.js
@@ -10,4 +10,4 @@ export const {
   LoginRequested,
   LoginSucceeded,
   LoginFail
-}
+} = Action
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
-//endpoint file 
+//endpoint file
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Provider } from 'react-redux'
+
 import App from './ui'
+import store from './store'
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
   document.getElementById('react')
 )
