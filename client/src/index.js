0b8f719d43648e28a78e96e36d30e73639ec427f
Kawin Glomjai <kawin.g@taskworld.com>
2016-08-04 16:56:27 +0700

    react-router-reducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 import App from './ui/App'
 import { Login } from './ui/login'
 import store from './store'
 
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={history}>
       <Route path='/' component={App}>
         <Route path='login' component={Login}/>
       </Route>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 
 import login from './login'
 
 export const rootReducer = combineReducers({
-  login
+  login,
+  routing: routerReducer
 })
+
+console.log(rootReducer)
 
 export default rootReducer
