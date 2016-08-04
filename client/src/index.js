cc5e45badec3509df94aa466357dd7df4c4de891
Kawin Glomjai <kawin.g@taskworld.com>
2016-08-04 16:52:57 +0700

    React router

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,21 @@
 //endpoint file
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-import App from './ui'
+import App from './ui/App'
+import { Login } from './ui/login'
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <Route path='login' component={Login}/>
+      </Route>
+    </Router>
   </Provider>
   ,
   document.getElementById('react')
