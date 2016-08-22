a618b8452a810c2f40aefdcf641b6844bc317db9
Kawin Glomjai <kawin.g@taskworld.com>
2016-08-22 15:23:32 +0700

    use configure routes

--- server/src/configureRoutes.js
+++ server/src/configureRoutes.js
@@ -1,5 +1,10 @@
-import Router from 'react-router'
 
-const configureRoutes = () => {
-  
+const configureRoutes = (request, response) => {
+  response.status(200)
+  response.render('index', {
+    asserts: 'http://localhost:2000/asserts',
+    appTitle: 'Com-Sci MJU'
+  })
 }
+
+export default configureRoutes
