b9439ce7636acd4f3f9d7bd76131e4a9f6d81115
Kawin Glomjai <kawin.g@taskworld.com>
2016-08-22 19:15:13 +0700

    Push rendered data to window.MrHotels

--- client/src/index.entry.js
+++ client/src/index.entry.js
@@ -1,6 +1,7 @@
 //endpoint file
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { renderToString } from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
@@ -14,14 +15,23 @@ require('font-awesome/css/font-awesome.css')
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store)
+const MrHotels = window.MrHotels = { }
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path='/' component={App} />
-      <Route path='/login' component={Login} />
-    </Router>
-  </Provider>
-  ,
-  document.getElementById('react')
-)
+function render (element) {
+  MrHotels.app = ReactDOM.render(element, document.getElementById('react'))
+}
+
+const bootstrapper = {
+  'bootApp': () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Route path='/' component={App} />
+          <Route path='/login' component={Login} />
+        </Router>
+      </Provider>
+    )
+  }
+}
+
+bootstrapper.bootApp()
