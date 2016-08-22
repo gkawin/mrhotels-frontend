efc439912cc274f07f2c31c937e14950caee3f7b
Kawin Glomjai <kawin.g@taskworld.com>
2016-08-22 15:23:03 +0700

    Restore

--- client/src/index.entry.js
+++ client/src/index.entry.js
@@ -2,16 +2,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 
+import App from './ui/App'
+import { Login } from './ui/login'
 import store from './store'
-import routes from './routes'
 
 // Import stylesheets to use
 require('font-awesome/css/font-awesome.css')
 
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 ReactDOM.render(
   <Provider store={store}>
-    {routes}
+    <Router history={history}>
+      <Route path='/' component={App} />
+      <Route path='/login' component={Login} />
+    </Router>
   </Provider>
   ,
   document.getElementById('react')
