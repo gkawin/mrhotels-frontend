d2739ea994a8234e1e591dab0d4f3968ed039e2a
Kawin Glomjai <kawin.g@taskworld.com>
2016-08-30 23:41:50 +0700

    Button

--- client/src/ui/uikit/Button.js
+++ client/src/ui/uikit/Button.js
@@ -1,14 +1,26 @@
 
 import React from 'react'
+import Classnames from 'classnames'
 
-const Button = ({ children }) => (
-  <button>
-    {children}
+import './Button.styl'
+
+export const classButton = ({ className, style, raised }) => Classnames('button', className, {
+  '--primary': style === 'primary'
+}, {
+  '--raised': raised
+})
+
+const Button = (props) => (
+  <button
+    className={classButton(props)}
+  >
+    {props.children}
   </button>
 )
 
 Button.propTypes = {
   children: React.PropTypes.node.isRequired,
+  className: React.PropTypes.string,
 }
 
 export default Button
--- client/src/ui/uikit/uikit.story.js
+++ client/src/ui/uikit/uikit.story.js
@@ -7,8 +7,34 @@ import {
 
 export { module }
 
+const FrontPageSimulator = ({ children }) => (
+  <div
+    style={{
+      position: 'relative',
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <div style={{backgroundColor: 'white', padding: '15px'}}>{children}</div>
+  </div>
+)
+
+FrontPageSimulator.propTypes = {
+  children: React.PropTypes.node
+}
+
 export const stories = {
-  'Button': () => (
-    <Button>foo bar</Button>
+  'Button primary': () => (
+    <FrontPageSimulator>
+      <Button
+        raised
+        style='primary'
+      >
+        Submit
+      </Button>
+    </FrontPageSimulator>
   )
 }
