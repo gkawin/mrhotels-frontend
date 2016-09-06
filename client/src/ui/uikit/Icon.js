db49a1f16ccc180abf3b99237779267b8a5c6f3a
Kawin Glomjai <kawin.g@taskworld.com>
2016-09-06 15:52:06 +0700

    Whole Icons Story

--- client/src/ui/uikit/Icon.js
+++ client/src/ui/uikit/Icon.js
@@ -1,29 +1,46 @@
+
+//NOTE: We're using https://github.com/andreypopp/react-fa is called  REACT FONT AWESOME
+//easy to use and integration.
+
 import React from 'react'
-import classnames from 'classnames'
+import ReactFontAwesome from 'react-fa'
+import Classnames from 'classnames'
+import _ from 'lodash'
 
 import './Icon.styl'
 
-const clsIconFeatures = (name, className) => classnames(
-  `icon-name__${name}`,
-  className
-)
+export const iconClassname = (className) => Classnames('icon', className)
 
-const Icon = ({ className, onClick, name }) => (
-  <i
-    className={clsIconFeatures(name, className)}
-    onClick={onClick}
+const propFields = [
+  'name', 'size', 'rotate', 'flip', //string
+  'pulse', 'inverse', 'spin' //boolean
+]
+
+export const getAllIcons = () => _(document.styleSheets)
+  .toArray()
+  .map('cssRules')
+  .map(_.toArray)
+  .flatten()
+  .map('selectorText')
+  .compact()
+  .map(selector => selector.match(/^\.fa-(.*?)::before/))
+  .compact()
+  .map(1)
+  .sortBy()
+  .value()
+
+const Icon = (props) => (
+  <ReactFontAwesome
+    className={iconClassname(props.className)}
+    onClick={props.onClick}
+    {..._.pick(props, propFields)}
   />
 )
 
-Icon.getAllIcons = () => {
-  return ''
-}
 
 Icon.propTypes = {
-  className: React.PropTypes.string,
   onClick: React.PropTypes.func,
-  name: React.PropTypes.string,
+  className: React.PropTypes.string,
 }
-
 
 export default Icon
--- client/src/ui/uikit/Icon.story.js
+++ client/src/ui/uikit/Icon.story.js
@@ -1,13 +1,26 @@
 
 import React from 'react'
-import Icon from './Icon'
+import Icon, { getAllIcons } from './Icon'
+import _ from 'lodash'
 
 export { module }
 
-export const stories = {
-  'all icons': () => (
-    <div>
-      {Icon.getAllIcons()}
+const iconWithName = (name, idx) => (
+  <div key={idx} style={{ padding: '20px', width: '40px', textAlign: 'center'}}>
+    <div style={{ textAlign: 'center' }}>
+      <Icon name={name} size='3x'  />
     </div>
-  )
+    <div style={{ fontSize: '12px', wordBreak: 'normal' }}>{name}</div>
+  </div>
+)
+
+const getAllNameIcons = getAllIcons()
+
+export const stories = {
+  'all Icons': () => (
+    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}>
+      {_.map(getAllNameIcons, iconWithName)}
+    </div>
+  ),
+
 }
