b648e15b212b1363216f54c37cf1af50da05157f
Kawin Glomjai <kawin.g@taskworld.com>
2016-12-08 12:16:40 +0700

    change content

--- server/src/constants.js
+++
@@ -1,8 +0,0 @@
-
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-export const BACKEND_API_SERVER = (
-  isDevelopment
-    ? 'http://localhost:30001'
-    : 'http://backend-api.mrhotels.co:2101'
-)
