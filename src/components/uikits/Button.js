efbd25e184ab3b67cc09d2ae407f9b4be00b15c1
Kawin Glomjai <kawin.g@taskworld.com>
2017-09-23 17:58:41 +0700

    classname

--- src/components/uikits/Button.js
+++ src/components/uikits/Button.js
@@ -1,6 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-const Button = styled.button`
+class Button extends React.PureComponent {
+  static propTypes = {
+    children: PropTypes.any,
+    className: PropTypes.string,
+    onClick: PropTypes.func
+  }
+  onClick = (e) => {
+    e.preventDefault()
+    this.props.onClick(e)
+  }
+  render () {
+    return (
+      <button className={this.props.className} onClick={this.onClick}>
+        {this.props.children}
+      </button>
+    )
+  }
+}
+
+const ButtonBaseStyle = styled(Button)`
   padding: 10px 20px;
   font-size: 16px;
   width: ${props => props.width || '200px'};
@@ -12,7 +33,8 @@ const Button = styled.button`
   cursor: pointer;
   transition: all 0.3s ease-in-out;
   &:hover {
-    background: ${props => props.theme.primary.main};
+    background: ${props => props.theme.secoundary.dark};
   }
 `
-export default Button
+
+export default ButtonBaseStyle
