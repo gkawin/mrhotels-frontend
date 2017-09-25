2921647455d6ac55c0bb00b302b1f28dcd147a80
Kawin Glomjai <kawin.g@taskworld.com>
2017-09-25 12:00:09 +0700

    decoration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,11 @@
 import './storybook.css'
 
-import { configure } from '@storybook/react'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { configure, addDecorator, setAddon } from '@storybook/react'
+import { theme } from '../src/design'
+
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>)
 
 function loadStories () {
   const context = require.context('../src', true, /\.story\.js$/)
--- src/components/layout/MainLayout.story.js
+++ src/components/layout/MainLayout.story.js
@@ -2,14 +2,11 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import MainLayout from './MainLayout'
-import Button from '../uikits/Button'
 
 storiesOf('MainLayout', module)
 .add('Red & Blue', () => (
   <MainLayout>
     <div>Click Me Baby</div>
-    <div>
-      <Button>Click Me Baby</Button>
-    </div>
+
   </MainLayout>
 ))
